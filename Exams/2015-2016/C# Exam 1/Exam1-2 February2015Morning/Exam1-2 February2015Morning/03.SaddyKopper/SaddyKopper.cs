c9b944958e9698b06f9757b995d8f446ca014d1a
Stefan Baltiyski <stefan.baltiyski@gmail.com>
2016-07-10 00:28:27 +0300

    Exam

--- Exams/2015-2016/C# Exam 1/Exam1-2 February2015Morning/Exam1-2 February2015Morning/03.SaddyKopper/SaddyKopper.cs
+++ Exams/2015-2016/C# Exam 1/Exam1-2 February2015Morning/Exam1-2 February2015Morning/03.SaddyKopper/SaddyKopper.cs
@@ -1,9 +1,39 @@
 ﻿using System;
+using System.Numerics;
 
 class SaddyKopper
 {
     static void Main(string[] args)
     {
+        string num = Console.ReadLine();
+        int transformationCount = 0;
 
+        
+        while (num.Length > 1 && transformationCount < 10)
+        {
+            BigInteger prod = 1;
+            while (num.Length > 0)
+            {
+                num = num.Substring(0, num.Length - 1);
+                int sum = 0;
+                for (int i = 0; i < num.Length; i += 2)
+                {
+                    sum += (num[i] - '0');
+                }
+
+                prod *= sum != 0 ? sum : 1;
+            }
+
+            transformationCount++;
+            num = prod.ToString();
+        }
+
+
+        if (transformationCount < 10)
+        {
+            Console.WriteLine(transformationCount);
+        }
+
+        Console.WriteLine(num);
     }
 }
