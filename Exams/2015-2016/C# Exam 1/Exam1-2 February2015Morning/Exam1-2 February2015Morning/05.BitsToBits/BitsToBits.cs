0da0924d5cf96129b44d398da9028270ae948085
Stefan Baltiyski <stefan.baltiyski@gmail.com>
2016-07-10 14:46:36 +0300

    Exam

--- Exams/2015-2016/C# Exam 1/Exam1-2 February2015Morning/Exam1-2 February2015Morning/05.BitsToBits/BitsToBits.cs
+++ Exams/2015-2016/C# Exam 1/Exam1-2 February2015Morning/Exam1-2 February2015Morning/05.BitsToBits/BitsToBits.cs
@@ -4,6 +4,55 @@ class BitsToBits
 {
     static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+
+        int maxZero = 0;
+        int currentZero = 0;
+
+        int maxOne = 0;
+        int currentOne = 0;
+
+        int lastBit = 5;
+
+        for (int i = 0; i < n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int j = 29; j >= 0; j--)
+            {
+                int bit = ((1 << j) & num) >> j;
+
+                if (bit == 1)
+                {
+                    if (lastBit == 1)
+                    {
+                        currentOne++;
+                        maxOne = Math.Max(maxOne, currentOne);
+                    }
+                    else
+                    {
+
+                        currentZero = 0;
+                        currentOne = 1;
+                    }
+                }
+                else
+                {
+                    if (lastBit == 0)
+                    {
+                        currentZero++;
+                        maxZero = Math.Max(maxZero, currentZero);
+                    }
+                    else
+                    {
+                        
+                        currentOne = 0;
+                        currentZero = 1;
+                    }
+                }
+                lastBit = bit;
+            }
+        }
+        Console.WriteLine(maxZero);
+        Console.WriteLine(maxOne);
     }
 }
