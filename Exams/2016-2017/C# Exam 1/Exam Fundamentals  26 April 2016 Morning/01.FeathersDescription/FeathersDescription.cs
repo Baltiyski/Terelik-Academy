aaa585a9bcd2a2f0938ccc886479f8b2c7ecfae9
Stefan Baltiyski <stefan.baltiyski@gmail.com>
2016-07-16 16:19:42 +0300

    Exam

---
+++ Exams/2016-2017/C# Exam 1/Exam Fundamentals  26 April 2016 Morning/01.FeathersDescription/FeathersDescription.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class FeathersDescription
+{
+    static void Main(string[] args)
+    {
+        double b = double.Parse(Console.ReadLine());
+        double f = double.Parse(Console.ReadLine());
+
+        if (b == 0)
+        {
+            Console.WriteLine("{0:F4}",b);
+        }
+        else
+        {
+            double sum = Math.Abs(f / b);
+            if (b % 2 != 0)
+            {
+                Console.WriteLine("{0:F4}", sum / 317);
+            }
+            else
+            {
+                Console.WriteLine("{0:F4}", sum * 123123123123);
+            }
+        }
+        
+    }
+}
---
+++ Exams/2016-2017/C# Exam 1/Exam Fundamentals  26 April 2016 Morning/02.BussesDescription/BussesDescription.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class BussesDescription
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int groupsCount = 1;
+
+        int firstInLineSpeed = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            int currentBusSpeed = int.Parse(Console.ReadLine());
+
+            if (currentBusSpeed <= firstInLineSpeed)
+            {
+                groupsCount++;
+                firstInLineSpeed = currentBusSpeed;
+            }
+        }
+
+        Console.WriteLine(groupsCount);
+    }
+}
