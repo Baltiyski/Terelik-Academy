fb1eda3e8d93fc8c6fc0640638aeec66a2131daf
Stefan Baltiyski <stefan.baltiyski@gmail.com>
2016-07-16 17:50:19 +0300

    2016 Exam

---
+++ Exams/2016-2017/C# Exam 1/Exam Fundamentals  26 April 2016 Morning/05.BobiAvokadoto/BobiAvokadoto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class BobiAvokadoto
+{
+    static void Main(string[] args)
+    {
+        uint n = uint.Parse(Console.ReadLine());
+        int howManyNum = int.Parse(Console.ReadLine());
+
+        uint bestNum = 0u;
+        uint bestBits = 0u;
+
+        for (int i = 0; i < howManyNum; i++)
+        {
+            uint num = uint.Parse(Console.ReadLine());
+            if ((n | num) == n + num)
+            {
+                uint bits = 0;
+                uint combo = num;
+
+                while (combo > 0)
+                {
+                    bits += combo & 1u;
+                    combo >>= 1;
+                }
+                if (bits > bestBits)
+                {
+                    bestNum = num;
+                    bestBits = bits;
+                }
+            }
+        }
+        Console.WriteLine(bestNum);
+    }
+}
