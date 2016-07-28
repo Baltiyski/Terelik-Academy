986f609708b7448853bef18de73f2e5bcd920ee0
Stefan Baltiyski <stefan.baltiyski@gmail.com>
2016-07-28 22:01:12 +0300

    Home Work

--- 02.C# Part 2/01.Arrays/11.BinarySearch/BinarySearch.cs
+++ 02.C# Part 2/01.Arrays/11.BinarySearch/BinarySearch.cs
@@ -4,6 +4,46 @@ class BinarySearch
 {
     static void Main(string[] args)
     {
+        int n = int.Parse(Console.ReadLine());
+        long[] array = new long[n];
 
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = long.Parse(Console.ReadLine());
+        }
+
+        int x = int.Parse(Console.ReadLine());
+        Array.Sort(array);
+
+
+        int startIndex = 0;
+        int endIndex = n - 1;
+        int midIndex = (endIndex - startIndex) / 2;
+        int count = 0;
+        int result = 0;
+
+        while(count <= n / 2)
+        {
+            if (array[midIndex] == x)
+            {
+                result = midIndex;
+                break;
+            }
+            else if (array[midIndex] < x)
+            {
+                startIndex = midIndex + 1;
+                midIndex = (startIndex + endIndex) / 2;
+                count++;
+            }
+            else
+            {
+                endIndex = midIndex - 1;
+                midIndex = (endIndex + startIndex) / 2;
+                count++;
+            }
+        }
+
+        Console.WriteLine(result);
+        
     }
 }
---
+++ 02.C# Part 2/01.Arrays/12.IndexOfLetters/IndexOfLetters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class IndexOfLetters
+{
+    static void Main(string[] args)
+    {
+        char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                               'u', 'v', 'w', 'x', 'y', 'z' };
+
+        string word = Console.ReadLine();
+
+        foreach (var leter in word)
+        {
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (leter == alphabet[i])
+                {
+                    Console.WriteLine(Array.IndexOf(alphabet, alphabet[i]));
+                }
+            }
+        }
+                            
+    }
+}
