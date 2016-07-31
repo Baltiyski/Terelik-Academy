96822e82afb4ad9da519e7f1af7fc1732881fb8c
Stefan Baltiyski <stefan.baltiyski@gmail.com>
2016-07-31 23:34:03 +0300

    Home Work

---
+++ 02.C# Part 2/01.Arrays/15.PrimeNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] list = new int[n + 1];
+            int newVal = 0;
+
+            for (int i = 2; i < list.Length; i++)
+            {
+                if (list[i] == 1)
+                {
+                    continue;
+                }
+
+                for (int j =  i*2; j < list.Length; j+=i)
+                {   
+                    list[j] = 1;   
+                }
+                newVal = i;
+            }
+
+            Console.WriteLine(newVal);
+        }
+    }
+}
---
+++ 02.C# Part 2/01.Arrays/16.SubsetWithSumS/SubsetWithSumS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 2, 1, 2, 4, 3, 5, 2, 6 };
+        var sum = 2;
+        var isSumExist = false;
+        var length = arr.Length;
+
+        int maxConbinations = (int)Math.Pow(2, length) - 1;
+
+        for (int i = 1; i <= maxConbinations; i++)
+        {
+            long currentsum = 0;
+            for (int j = 0; j < length; j++)
+            {
+
+                int mask = 1 << j;
+                int bit = (mask & i) >> j;
+
+                if (bit == 1)
+                {
+                    currentsum += arr[j];
+                }
+            }
+
+            if (currentsum == sum)
+            {
+                isSumExist = true;
+                break;
+            }
+        }
+
+        var output = isSumExist ? "yes" : "no";
+        Console.WriteLine(output);
+    }
+}
