5ce1c0de2f06d468c26b7cc22330b3cb865101a8
Stefan Baltiyski <stefan.baltiyski@gmail.com>
2016-08-07 15:54:28 +0300

    Home Work 2016

---
+++ 02.C# Part 2/02.MultidimentionalArrays/01.FillTheMatri/FillTheMatri.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+class FillTheMatri
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[,] array = new int[n, n];
+        char letter = char.Parse(Console.ReadLine());
+
+        int row = array.GetLength(0);
+        int coll = array.GetLength(1);
+
+        switch (letter)
+        {
+            case 'a':
+                for (int i = 0; i < row; i++)
+                { 
+                    for (int j = 0; j < coll; j++)
+                    {
+                        if (j == 0)
+                        {
+                            array[i, j] = i + 1;
+                        }
+                        else
+                        {
+                            array[i, j] = array[i, j - 1] + n;
+                        }           
+                    }
+                }
+                break;
+            case 'b':
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < coll; j++)
+                    {
+                        if (j == 0)
+                        {
+                            array[i, j] = i + 1;
+                        }
+                        else if(j % 2 != 0)
+                        {
+                            array[i, j] = n * (j + 1) - i;
+                        }
+                        else if(j % 2 == 0)
+                        {
+                            array[i, j] = (array[i, j - 1] + i) + array[i,0];
+                        }
+                    }
+                }
+                break;
+            case 'c':
+                int d = 1;
+                for (int i = row; i > 0; i--)
+                {
+                    for (int j = 0; j < coll; j++)
+                    {
+                        if (j == 0)
+                        {
+                            if (i == row)
+                            {
+                                array[i - 1, j] = array[i - 1, j] + 1;
+                            }
+                            else
+                            {
+                                array[i -1 , j] = array[i, j] + d;
+                                d++;
+                            }
+                            
+                        }
+                        if(i == 1 && j > 0)
+                        {
+                            array[0, j] = array[0, j - 1] + n - (j - 1); 
+                        }
+                    }
+                }
+
+                for (int i = 1; i < row; i++)
+                {
+                    for (int j = 1; j < coll; j++)
+                    {
+                        array[i, j] = array[i - 1, j - 1] + 1;
+                    }
+                }
+                
+                break;
+            case 'd':
+                int firstColaps = 0;
+                int lastColMinus = 1;
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < coll; j++)
+                    {
+                        if (firstColaps < row)
+                        {
+                            if (j == 0)
+                            {
+                                array[i, j] = i + 1;
+                            }
+                            if (i == row - 1 && j > 0)
+                            {
+                                array[i, j] = array[i, j - 1] + 1;
+                                if (j == coll - 1)
+                                {
+                                    i = 0;
+                                }
+                            }
+                        }
+                        else if(firstColaps < row + n - 1)
+                        {
+                            if (j== coll - 1 )
+                            {
+                                array[i - 1, j] = array[row - 1, coll - 1] + n - i;
+                                if (i == row -1)
+                                {
+                                    i = 0;
+                                }
+                            }
+                        }
+                    }
+                    firstColaps++;
+                }
+                break;
+            default:
+                break;
+        }
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < coll; j++)
+            {
+                if (j > 0)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(array[i,j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 02.C# Part 2/02.MultidimentionalArrays/02.MaimalSum/MaimalSum.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MaimalSum
+{
+    static void Main(string[] args)
+    {
+        int[] size = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int rows = size.First();
+        int cols = size.Last();
+
+        int[,] matrix = new int[rows, cols];
+
+        for (int row = 0; row < rows; row++)
+        {
+            string[] currentRowAsString = Console.ReadLine().Split(' ');
+
+            for (int col = 0; col < cols; col++)
+            {
+                matrix[row, col] = int.Parse(currentRowAsString[col]);
+            }
+        }
+
+        int bestSum = int.MinValue;
+        int[,] maxSumMatrix = new int[3, 3];
+
+
+        for (int row = 0; row < rows - 2; row++)
+        {
+            for (int col = 0; col < cols - 2; col++)
+            {
+                int currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2]
+                    + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2]
+                    + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                if (currentSum > bestSum)
+                {
+                    bestSum = currentSum;
+
+                    for (int i = 0; i < maxSumMatrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < maxSumMatrix.GetLength(1); j++)
+                        {
+                            maxSumMatrix[i, j] = matrix[row + i, col + j];
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine( bestSum);
+
+    }
+}
